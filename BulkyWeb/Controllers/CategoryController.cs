aef284f1162f084a3c33323a4d4243fa71303516
Jesus Agudo <jagudo2514@gmail.com>
2026-01-19 22:44:33 -0500

    CRUD EF

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -44,13 +44,100 @@ namespace BulkyWeb.Controllers
                 ModelState.AddModelError("DisplayOrder", "A category with this Display Order already exists.");
             }
 
+            //Validate without model attribute
+            // ModelState.AddModelError("", "Custom error message without specific field.");
+
             if (ModelState.IsValid)
             {
                 _dbContext.Categories.Add(category);
                 _dbContext.SaveChanges();
+                TempData["success"] = "Category created successfully!";
                 return RedirectToAction("Index");
             }
             return View(category);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            //Category? categoryFromDb = _dbContext.Categories.Find(id);
+            //Category? categoryFromDb = _dbContext.Categories.SingleOrDefault(c => c.Id == id);
+            //Difference between FirstOrDefault and SingleOrDefault: FirstOrDefault returns the first matching element or a default value if no match is found, while SingleOrDefault expects exactly one matching element and throws an exception if there are multiple matches.
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+
+            // Server-side validation
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+            }
+
+            //Validate if exists in database display order != id
+
+            //Validate without model attribute
+            // ModelState.AddModelError("", "Custom error message without specific field.");
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            //Category? categoryFromDb = _dbContext.Categories.Find(id);
+            //Category? categoryFromDb = _dbContext.Categories.SingleOrDefault(c => c.Id == id);
+            //Difference between FirstOrDefault and SingleOrDefault: FirstOrDefault returns the first matching element or a default value if no match is found, while SingleOrDefault expects exactly one matching element and throws an exception if there are multiple matches.
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Categories.Remove(categoryFromDb);
+            _dbContext.SaveChanges();
+            TempData["success"] = "Category deleted successfully!";
+            return RedirectToAction("Index");
+        }
+
     }
 }
